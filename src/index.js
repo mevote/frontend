049e8903923e94b2b64f1de349bf949aab4ce179
jooooonis <xowns3213@naver.com>
2022-05-20 17:13:32 +0900

    Feat : css reset 파일 추가 css 초기화를 위한 Reset.jsx을 작성하고 index.js에 적용하였습니다.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Reset from './styles/Reset';
 import Home from './Home';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Reset />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/test" element={<Home />} />
