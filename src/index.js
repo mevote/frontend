77d7d09c2c183799a65001416f7c2d1c84b2910b
jooooonis <xowns3213@naver.com>
2022-05-20 23:49:38 +0900

    Fix : 라우터 수정 후보 페이지 두개를 라우터에 추가하였습니다.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Reset from './styles/Reset';
-import Candidate from './Candidate';
 import Home from './Home';
+import CandidateInformation from './Candidate/CandidateInformation';
+import CandidatePromise from './Candidate/CandidatePromise';
 import Polls from './Polls';
 
 ReactDOM.render(
@@ -11,7 +12,8 @@ ReactDOM.render(
     <Reset />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/candidate" element={<Candidate />} />
+      <Route path="/candidateinformation" element={<CandidateInformation />} />
+      <Route path="/candidatepromise" element={<CandidatePromise />} />
       <Route path="/polls" element={<Polls />} />
     </Routes>
   </BrowserRouter>,
