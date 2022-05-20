8ce51099e8122aa7247fc7a37ecfd692cb2afe39
jooooonis <xowns3213@naver.com>
2022-05-20 16:23:15 +0900

    Chore : Home css 수정 div가 화면 가운데로 오도록 css를 수정하였습니다.

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,9 +4,10 @@ import Header from './Header';
 
 const Container = styled.div`
   display: flex;
-  position: absolute;
-  top: 0;
-  left: 0;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
   max-width: 1020px;
 `;
 
@@ -14,6 +15,10 @@ function Home() {
   return (
     <Container>
       <Header />
+      <div>
+        <h1>안녕하세요 여기는 h1입니다</h1>
+        <p>안녕하세요 여기는 p입니다.</p>
+      </div>
     </Container>
   );
 }
