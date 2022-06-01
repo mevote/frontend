4e3d87042d221867969e8f030a2f79848a1e96cb
wonkyum-kim <dodo12120@naver.com>
2022-06-01 17:19:43 +0900

    Test : tts api 사용방법을 주석으로 적어두었습니다.

--- src/Candidate/CandidatePromise.jsx
+++ src/Candidate/CandidatePromise.jsx
@@ -11,18 +11,6 @@ const Container = styled.div`
   margin: auto;
 `;
 
-/* 
-const PromiseContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  border-style: solid;
-  marign: 20px;
-  height: 30vh;
-  border-radius: 50px;
-`;
-*/
-
 const DetailedPromise = styled.div`
   font-size: 20px;
   text-align: left;
@@ -32,10 +20,36 @@ const DetailedPromise = styled.div`
   white-space: pre-wrap;
 `;
 
+// tts service key
+// 086659abbee941cda0bb9d7594bc8581
+
+/*
+ * api 사이트
+ * https://rapidapi.com/voicerss/api/text-to-speech-1/
+ * https://www.voicerss.org/api/
+ *
+ * 자바스크립트 예시 코드
+ * https://codepen.io/SitePoint/pen/JRaLVR
+ */
+
+/*
+async function foo() {
+  let url = 'https://api.voicerss.org/?';
+  let key = '086659abbee941cda0bb9d7594bc8581';
+  let queryParams = encodeURIComponent('key') + '=' + key;
+  let str = encodeURIComponent('안녕하세요\n감사합니다.');
+  queryParams += '&' + encodeURIComponent('src') + '=' + str;
+  queryParams += '&' + encodeURIComponent('hl') + '=' + encodeURIComponent('ko-kr');
+  queryParams += '&' + encodeURIComponent('c') + '=' + encodeURIComponent('mp3');
+
+  // let response = await fetch(url + queryParams);
+  console.log(url + queryParams);
+}
+*/
+
 // 후보 공약 페이지
 
 const HuboInfo = ({ p1, p2, p3, p4, p5 }) => {
-  console.log(p1);
   return (
     <div>
       <DetailedPromise>{p1}</DetailedPromise>
